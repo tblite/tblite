3d1053b8739a44ee519b604ca6d00e4d88f1b5c3
Pit Steinbach <steinbach@pc.rwth-aachen.de>
2023-09-28 11:58:46 +0200

    renaming and adding license info

--- src/tblite/double_dictionary.f90
+++ src/tblite/double_dictionary.f90
@@ -1,3 +1,24 @@
+! This file is part of tblite.
+! SPDX-Identifier: LGPL-3.0-or-later
+!
+! tblite is free software: you can redistribute it and/or modify it under
+! the terms of the GNU Lesser General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! tblite is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU Lesser General Public License for more details.
+!
+! You should have received a copy of the GNU Lesser General Public License
+! along with tblite.  If not, see <https://www.gnu.org/licenses/>.
+
+!> @dir tblite/double_dictionary
+!> Contains implemenation of a dictionary with strings as keys and arrays of doubles as entries.
+
+!> @file tblite/double_dictionery.f90
+!> Implements double dictionary type
 module tblite_double_dictionary
    use mctc_env_accuracy, only : wp, i8
    implicit none
