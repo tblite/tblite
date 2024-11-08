a4589b471cd433699c3810723628390d0ed8fe6b
thfroitzheim <92028749+thfroitzheim@users.noreply.github.com>
2024-11-08 14:59:14 +0100

    Update all dependencies consistently for meson, fpm and cmake

--- config/cmake/Findmulticharge.cmake
+++ config/cmake/Findmulticharge.cmake
@@ -17,6 +17,7 @@
 set(_lib "multicharge")
 set(_pkg "MULTICHARGE")
 set(_url "https://github.com/grimme-lab/multicharge")
+set(_rev "v0.3.0")
 
 if(NOT DEFINED "${_pkg}_FIND_METHOD")
   if(DEFINED "${PROJECT_NAME}-dependency-method")
@@ -29,7 +30,7 @@ endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/tblite-utils.cmake")
 
-tblite_find_package("${_lib}" "${${_pkg}_FIND_METHOD}" "${_url}" "v0.2.0")
+tblite_find_package("${_lib}" "${${_pkg}_FIND_METHOD}" "${_url}" "${_rev}")
 
 if(DEFINED "_${_pkg}_FIND_METHOD")
   unset("${_pkg}_FIND_METHOD")
