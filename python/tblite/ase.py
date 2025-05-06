d4fb2f948c62d923851d855b9d5c84f45c0aface
Sebastian Ehlert <sehlert@microsoft.com>
2025-05-06 11:20:20 +0200

    Update documentation for ASE calculator

--- python/tblite/ase.py
+++ python/tblite/ase.py
@@ -14,13 +14,71 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with tblite.  If not, see <https://www.gnu.org/licenses/>.
 """
-The Python API of *tblite* natively support integration with the atomic 
+The Python API of *tblite* natively support integration with the atomic
 simulation environment (`ASE`_).
 By constructing a calculator most functionality of ASE is readily available.
 For details on building the Python API checkout the
 :ref:`installation guide <python-build>`.
 
 .. _ase: https://wiki.fysik.dtu.dk/ase/
+
+Example
+-------
+
+The calculator can be used like other ASE calculators with by attaching it to an atoms object.
+
+>>> from ase.build import molecule
+>>> from tblite.ase import TBLite
+>>> atoms = molecule("H2O")
+>>> atoms.calc = TBLite(method="GFN2-xTB")
+>>> atoms.get_potential_energy()  # in eV
+-137.96777594361677
+
+The calculator will perform single point calculations as necessary, if properties for
+the same atoms are requested the properties will be returned without additional single point
+calculations.
+
+>>> atoms.get_dipole_moment()  # in Debye
+array([-7.83154860e-17,  1.10157136e-16, -4.76020974e-01])
+>>> atoms.get_forces()  # in eV/A
+array([[-3.31859412e-15,  7.83871951e-15, -7.49527168e-01],
+       [ 3.08351626e-15, -1.53506496e-01,  3.74763584e-01],
+       [ 2.35077857e-16,  1.53506496e-01,  3.74763584e-01]])
+
+Changed properties of the atoms object will lead to an reevalution of the single point,
+for example updating the initial charges of the atoms to set a total charge.
+
+>>> atoms.set_initial_charges([0, 0, -1])
+>>> atoms.get_potential_energy()  # in eV
+-131.50015843891816
+
+Some properties, like the total charge, can also be set via the parameters of the
+calculator.
+
+>>> atoms = molecule("H2O")
+>>> atoms.calc = TBLite(method="GFN2-xTB", charge=-1)
+>>> atoms.get_potential_energy()  # in eV
+-131.50015843891822
+
+Finally, the parameters of the calculator can be updated at any time using the *set*
+method, which resets the calculation as necessary.
+
+>>> atoms.calc.set(method="GFN1-xTB", charge=0)
+>>> atoms.get_potential_energy()  # in eV
+-156.96750590276173
+>>> atoms.get_dipole_moment()  # in Debye
+array([ 3.71500789e-16,  3.08439980e-16, -5.97048616e-01])
+>>> atoms.get_forces()  # in eV/A
+array([[ 2.88997963e-15,  5.70620869e-15, -7.84252969e-01],
+       [-4.90812472e-15, -2.28835628e-01,  3.92126485e-01],
+       [ 2.01814509e-15,  2.28835628e-01,  3.92126485e-01]])
+
+The calculator also support atom partitioned properties, including energies and charges.
+
+>>> atoms.get_charges()  # in e
+array([-0.66558478,  0.33279239,  0.33279239])
+>>> atoms.get_potential_energies()  # in eV
+array([-132.37935247,  -12.29407672,  -12.29407672])
 """
 
 try:
@@ -31,7 +89,7 @@ try:
 except ModuleNotFoundError as e:
     raise ModuleNotFoundError("This submodule requires ASE installed") from e
 
-from typing import List, Optional
+from typing import Any, Dict, List, Optional
 
 from .interface import Calculator
 
@@ -41,11 +99,12 @@ class TBLite(ase.calculators.calculator.Calculator):
     ASE calculator for using xTB Hamiltonians from the tblite library.
     Supported properties by this calculator are:
 
-    - energy (free_energy)
-    - forces
-    - stress
-    - dipole
-    - charges
+    - *energy* (free_energy): The total energy of the system (in eV)
+    - *energies*: The atom-partitioned energy of the system (in eV)
+    - *forces*: The derivative of the energy with respect to the atomic positions (in eV/Ang)
+    - *stress*: The derivative of the energy with respect to cell deformations (PBC only, in eV/Ang³)
+    - *dipole*: The dipole moment of the system (in Debye)
+    - *charges*: Mulliken charges of the system (in e)
 
     Supported keywords are
 
@@ -62,14 +121,24 @@ class TBLite(ase.calculators.calculator.Calculator):
      mixer_damping            0.4               Damping parameter for self-consistent mixer
      electric_field           None              Uniform electric field vector (in V/A)
      spin_polarization        None              Spin polarization (scaling factor)
-     alpb_solvation           None              Solvent name, solution state (optional)
-     gbsa_solvation           None              Solvent name, solution state (optional)
-     cpcm_solvation           None              Epsilon
-     gbe_solvation            None              Epsilon, Born kernel
-     gb_solvation             None              Epsilon, Born kernel
+     solvation                None              Solvation model to use (see below for details)
      cache_api                True              Reuse generate API objects (recommended)
      verbosity                1                 Set verbosity of printout
     ======================== ================= ====================================================
+
+    Solvation models are supported by passing a tuple to the *solvation* keyword.
+    The first element of the tuple is the name of the solvation model, the following
+    arguments are passed to initialize the solvation model.
+
+    ================== ====================================================
+     Solvation model    Arguments
+    ================== ====================================================
+     "alpb"             Solvent name (str), solution state (optional)
+     "gbsa"             Solvent name (str), solution state (optional)
+     "cpcm"             Epsilon (float)
+     "gbe"              Epsilon (float), Born kernel
+     "gb"               Epsilon (float), Born kernel
+    ================== ====================================================
 
     Example
     -------
@@ -103,7 +172,7 @@ class TBLite(ase.calculators.calculator.Calculator):
     ...     cell=np.array([5.68032, 5.68032, 5.68032]),
     ...     pbc=np.array([True, True, True]),
     ... )
-    >>> atoms.calc = TBLite(method="GFN1-xTB")
+    >>> atoms.calc = TBLite(method="GFN1-xTB", verbosity=0)
     >>> atoms.get_potential_energy()  # result in eV
     -1257.0943962462964
 
@@ -112,6 +181,7 @@ class TBLite(ase.calculators.calculator.Calculator):
 
     implemented_properties = [
         "energy",
+        "energies",
         "forces",
         "charges",
         "dipole",
@@ -128,11 +198,7 @@ class TBLite(ase.calculators.calculator.Calculator):
         "mixer_damping": 0.4,
         "electric_field": None,
         "spin_polarization": None,
-        "alpb_solvation": None,
-        "gbsa_solvation": None,
-        "cpcm_solvation": None,
-        "gbe_solvation": None,
-        "gb_solvation": None,
+        "solvation": None,
         "electronic_temperature": 300.0,
         "cache_api": True,
         "verbosity": 1,
@@ -150,9 +216,7 @@ class TBLite(ase.calculators.calculator.Calculator):
         Construct the TBLite base calculator object.
         """
 
-        ase.calculators.calculator.Calculator.__init__(
-            self, atoms=atoms, **kwargs
-        )
+        ase.calculators.calculator.Calculator.__init__(self, atoms=atoms, **kwargs)
 
     def set(self, **kwargs) -> dict:
         """
@@ -173,9 +237,9 @@ class TBLite(ase.calculators.calculator.Calculator):
         -156.9675057724589
         """
 
-        changed_parameters = ase.calculators.calculator.Calculator.set(
-            self, **kwargs
-        )
+        _update_parameters(kwargs)
+
+        changed_parameters = ase.calculators.calculator.Calculator.set(self, **kwargs)
 
         # Always reset the calculation if parameters change
         if changed_parameters:
@@ -186,11 +250,7 @@ class TBLite(ase.calculators.calculator.Calculator):
             "method" in changed_parameters
             or "electric_field" in changed_parameters
             or "spin_polarization" in changed_parameters
-            or "alpb_solvation" in changed_parameters
-            or "gbsa_solvation" in changed_parameters
-            or "cpcm_solvation" in changed_parameters
-            or "gbe_solvation" in changed_parameters
-            or "gb_solvation" in changed_parameters
+            or "solvation" in changed_parameters
         ):
             self._xtb = None
             self._res = None
@@ -210,12 +270,18 @@ class TBLite(ase.calculators.calculator.Calculator):
                 self._xtb.set("max-iter", self.parameters.max_iterations)
 
             if "initial_guess" in changed_parameters:
-                self._xtb.set(
-                    "guess", {"sad": 0, "eeq": 1}[self.parameters.guess]
-                )
+                self._xtb.set("guess", {"sad": 0, "eeq": 1}[self.parameters.guess])
 
             if "mixer_damping" in changed_parameters:
                 self._xtb.set("mixer-damping", self.parameters.mixer_damping)
+
+            if (
+                "charge" in changed_parameters or "multiplicity" in changed_parameters
+            ) and self.atoms is not None:
+                self._xtb.update(
+                    charge=_get_charge(self.atoms, self.parameters),
+                    uhf=_get_uhf(self.atoms, self.parameters),
+                )
 
         return changed_parameters
 
@@ -230,31 +296,14 @@ class TBLite(ase.calculators.calculator.Calculator):
             self._xtb = None
             self._res = None
 
-    @property
-    def _charge(self) -> int:
-        return (
-            self.atoms.get_initial_charges().sum()
-            if self.parameters.charge is None
-            else self.parameters.charge
-        )
-
-    @property
-    def _uhf(self) -> int:
-        return (
-            int(self.atoms.get_initial_magnetic_moments().sum().round())
-            if self.parameters.multiplicity is None
-            else self.parameters.multiplicity - 1
-        )
-
     def _check_api_calculator(self, system_changes: List[str]) -> None:
         """Check state of API calculator and reset if necessary"""
 
-        # Changes in positions and cell parameters can use a normal update
+        # Changes in positions, cell, charges and magnetic moments can use a normal update
         _reset = system_changes.copy()
-        if "positions" in _reset:
-            _reset.remove("positions")
-        if "cell" in _reset:
-            _reset.remove("cell")
+        for _change in system_changes:
+            if _change in ("positions", "cell", "initial_charges", "initial_magmoms"):
+                _reset.remove(_change)
 
         # Invalidate cached calculator and results object
         if _reset:
@@ -265,70 +314,16 @@ class TBLite(ase.calculators.calculator.Calculator):
                 try:
                     _cell = self.atoms.cell
                     self._xtb.update(
-                        self.atoms.positions / Bohr,
-                        _cell / Bohr,
+                        positions=self.atoms.positions / Bohr,
+                        lattice=_cell / Bohr,
+                        charge=_get_charge(self.atoms, self.parameters),
+                        uhf=_get_uhf(self.atoms, self.parameters),
                     )
                 # An exception in this part means the geometry is bad,
                 # still we will give a complete reset a try as well
                 except RuntimeError:
                     self._xtb = None
                     self._res = None
-
-    def _create_api_calculator(self) -> Calculator:
-        """Create a new API calculator object"""
-
-        try:
-            _cell = self.atoms.cell
-            _periodic = self.atoms.pbc
-            _charge = self._charge
-            _uhf = self._uhf
-
-            calc = Calculator(
-                self.parameters.method,
-                self.atoms.numbers,
-                self.atoms.positions / Bohr,
-                _charge,
-                _uhf,
-                _cell / Bohr,
-                _periodic,
-            )
-            calc.set("accuracy", self.parameters.accuracy)
-            calc.set(
-                "temperature",
-                self.parameters.electronic_temperature * kB / Hartree,
-            )
-            calc.set("max-iter", self.parameters.max_iterations)
-            calc.set("guess", {"sad": 0, "eeq": 1}[self.parameters.guess])
-            calc.set("mixer-damping", self.parameters.mixer_damping)
-            calc.set("verbosity", self.parameters.verbosity)
-            if self.parameters.electric_field is not None:
-                calc.add(
-                    "electric-field",
-                    self.parameters.electric_field * Bohr / Hartree,
-                )
-            if self.parameters.spin_polarization is not None:
-                calc.add("spin-polarization", self.parameters.spin_polarization)
-            if self.parameters.alpb_solvation is not None:
-                if isinstance(self.parameters.alpb_solvation, (tuple, list)):
-                    calc.add("alpb-solvation", *self.parameters.alpb_solvation)
-                else:
-                    calc.add("alpb-solvation", self.parameters.alpb_solvation)
-            if self.parameters.gbsa_solvation is not None:
-                if isinstance(self.parameters.gbsa_solvation, (tuple, list)):
-                    calc.add("gbsa-solvation", *self.parameters.gbsa_solvation)
-                else:
-                    calc.add("gbsa-solvation", self.parameters.gbsa_solvation)
-            if self.parameters.cpcm_solvation is not None:
-                calc.add("cpcm-solvation", self.parameters.cpcm_solvation)
-            if self.parameters.gbe_solvation is not None:
-                calc.add("gbe-solvation", *self.parameters.gbe_solvation)
-            if self.parameters.gb_solvation is not None:
-                calc.add("gb-solvation", *self.parameters.gb_solvation)
-
-        except RuntimeError as e:
-            raise ase.calculators.calculator.InputError(str(e)) from e
-
-        return calc
 
     def calculate(
         self,
@@ -363,14 +358,12 @@ class TBLite(ase.calculators.calculator.Calculator):
 
         if not properties:
             properties = ["energy"]
-        ase.calculators.calculator.Calculator.calculate(
-            self, atoms, properties, system_changes
-        )
+        ase.calculators.calculator.Calculator.calculate(self, atoms, properties, system_changes)
 
         self._check_api_calculator(system_changes)
 
         if self._xtb is None:
-            self._xtb = self._create_api_calculator()
+            self._xtb = _create_api_calculator(self.atoms, self.parameters)
 
         try:
             self._res = self._xtb.singlepoint(self._res)
@@ -378,15 +371,118 @@ class TBLite(ase.calculators.calculator.Calculator):
             raise ase.calculators.calculator.CalculationFailed(str(e)) from e
 
         # These properties are garanteed to exist for all implemented calculators
-        self.results["energy"] = self._res.get("energy") * Hartree
+        self.results["energy"] = self._res["energy"] * Hartree
+        self.results["energies"] = self._res["energies"] * Hartree
         self.results["free_energy"] = self.results["energy"]
-        self.results["forces"] = -self._res.get("gradient") * Hartree / Bohr
-        self.results["charges"] = self._res.get("charges")
-        self.results["dipole"] = self._res.get("dipole") * Bohr
-        self.results["bond-orders"] = self._res.get("bond-orders")
+        self.results["forces"] = -self._res["gradient"] * Hartree / Bohr
+        self.results["charges"] = self._res["charges"]
+        self.results["dipole"] = self._res["dipole"] * Bohr
+        self.results["bond-orders"] = self._res["bond-orders"]
         # stress tensor is only returned for periodic systems
         if self.atoms.pbc.any():
-            _stress = (
-                self._res.get("virial") * Hartree / self.atoms.get_volume()
-            )
+            _stress = self._res["virial"] * Hartree / self.atoms.get_volume()
             self.results["stress"] = _stress.flat[[0, 4, 8, 5, 2, 1]]
+
+
+def _create_api_calculator(
+    atoms: ase.Atoms,
+    parameters: ase.calculators.calculator.Parameters,
+) -> Calculator:
+    """Create a new API calculator object"""
+
+    try:
+        _cell = atoms.cell
+        _periodic = atoms.pbc
+        _charge = _get_charge(atoms, parameters)
+        _uhf = _get_uhf(atoms, parameters)
+
+        calc = Calculator(
+            parameters.method,
+            atoms.numbers,
+            atoms.positions / Bohr,
+            _charge,
+            _uhf,
+            _cell / Bohr,
+            _periodic,
+        )
+        calc.set("accuracy", parameters.accuracy)
+        calc.set(
+            "temperature",
+            parameters.electronic_temperature * kB / Hartree,
+        )
+        calc.set("max-iter", parameters.max_iterations)
+        calc.set("guess", {"sad": 0, "eeq": 1}[parameters.guess])
+        calc.set("mixer-damping", parameters.mixer_damping)
+        calc.set("verbosity", parameters.verbosity)
+        if parameters.electric_field is not None:
+            calc.add(
+                "electric-field",
+                parameters.electric_field * Bohr / Hartree,
+            )
+        if parameters.spin_polarization is not None:
+            calc.add("spin-polarization", parameters.spin_polarization)
+        if parameters.solvation is not None:
+            solvation_model, *solvation_args = parameters.solvation
+            if isinstance(solvation_args, (tuple, list)):
+                calc.add(f"{solvation_model}-solvation", *solvation_args)
+            else:
+                calc.add(f"{solvation_model}-solvation", solvation_args)
+
+    except RuntimeError as e:
+        raise ase.calculators.calculator.InputError(str(e)) from e
+
+    return calc
+
+
+def _get_charge(atoms: ase.Atoms, parameters: ase.calculators.calculator.Parameters) -> int:
+    """
+    Get the total charge of the system.
+    If no charge is provided, the total charge of the system is calculated
+    by summing the initial charges of all atoms.
+    """
+    return atoms.get_initial_charges().sum() if parameters.charge is None else parameters.charge
+
+
+def _get_uhf(atoms: ase.Atoms, parameters: ase.calculators.calculator.Parameters) -> int:
+    """
+    Get the number of unpaired electrons.
+    If no multiplicity is provided, the number of unpaired electrons
+    is calculated by summing the initial magnetic moments of all atoms.
+    """
+    return (
+        int(atoms.get_initial_magnetic_moments().sum().round())
+        if parameters.multiplicity is None
+        else parameters.multiplicity - 1
+    )
+
+
+def _update_parameters(kwargs: Dict[str, Any]) -> None:
+    """
+    Update the parameters of the calculator with the provided keyword arguments.
+    This function is used to update the parameters of the calculator
+    when new values are provided.
+    """
+
+    for key in (
+        "alpb",
+        "gbsa",
+        "cpcm",
+        "gbe",
+        "gb",
+    ):
+        if f"{key}_solvation" in kwargs:
+            value = kwargs.pop(f"{key}_solvation")
+            if isinstance(value, (tuple, list)):
+                value = (key, *value)
+            else:
+                value = (key, value)
+
+            if "solvation" in kwargs:
+                raise ase.calculators.calculator.InputError(
+                    "Multiple solvation models provided, can only use one"
+                )
+            kwargs["solvation"] = value
+
+
+if "tblite" not in ase.calculators.calculator.external_calculators:
+    ase.calculators.calculator.register_calculator_class("tblite", TBLite)
